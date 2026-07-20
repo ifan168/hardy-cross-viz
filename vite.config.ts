2369f73ef8dc07d0d62793b0f06dfe926cae28b8
ifan168 <48313563+ifan168@users.noreply.github.com>
2026-07-20 17:58:12 +0800

    Deploy app with GitHub Pages (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // GitHub Pages 将站点部署在仓库同名子路径下。
+  base: "/hardy-cross-viz/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
